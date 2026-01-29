cf744c5d860c2d34e90290af6c483e014bda6f1c
leandro <ljpcarvalho@gmail.com>
2026-01-29 01:01:42 -0300

    chore: modulos de exportação

---
+++ shared/src/DTO/index.ts
@@ -0,0 +1,3 @@
+export * from "./BackSocket.js";
+export * from "./FrontSocket.js";
+export * from "./Usuario.js";
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./DTO/index.js";
 export * from "./interface.js";
 export * from "./enum.js";
 export * from "./type.js";
